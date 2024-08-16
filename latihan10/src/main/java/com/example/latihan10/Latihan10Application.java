690ca49114380bb368d2fcc0e077d9bc7bed2c38
ssahlilj <46700670+savakun12@users.noreply.github.com>
2024-08-16 17:28:34 +0700

    java latihan 10
    java latihan 10

---
+++ latihan10/src/main/java/com/example/latihan10/Latihan10Application.java
@@ -0,0 +1,13 @@
+package com.example.latihan10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Latihan10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Latihan10Application.class, args);
+	}
+
+}
---
+++ latihan10/src/test/java/com/example/latihan10/Latihan10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latihan10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Latihan10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
